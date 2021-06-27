07fa722d4a20023e6d8c16888d4f54805a0388d7
Pineape123 <83844501+Pineape123@users.noreply.github.com>
2021-06-26 18:12:41 -0600

    mute command maybe?

--- cogs/mute.py
+++ cogs/mute.py
@@ -1,16 +1,23 @@
-import discord
+import asyncio
 from discord.ext import commands
+import discord
 
 class mute(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-
-	
-	@commands.command()
-	async def mute(self, ctx, user:discord.Member, *, reason=None, duration=None):
-		role = discord.utils.get(ctx.guild.roles, name="Muted")
-
-		await user.add_roles(role)
+	@commands.command
+	async def mute(self, ctx, user : discord.Member, duration = 0,*, unit = None):
+		roleobject = discord.utils.get(ctx.message.guild.roles, id="Muted")
+		await ctx.send(f":white_check_mark: Muted {user} for {duration}{unit}")
+		await user.add_roles(roleobject)
+		if unit == "s":
+			wait = 1 * duration
+			await asyncio.sleep(wait)
+		elif unit == "m":
+			wait = 60 * duration
+			await asyncio.sleep(wait)
+		await user.remove_roles(roleobject)
+		await ctx.send(f":white_check_mark: {user} was unmuted")
 
 def setup(bot):
 	bot.add_cog(mute(bot))
