c5418cec5990fda56c468cd8375de8541a767b3c
Sriyans Kanneganti <sriyansgamer@gmail.com>
2021-07-01 18:16:33 -0700

    Modified sum grammar and stuff. I've a lot more to go bois.

--- cogs/bane.py
+++ cogs/bane.py
@@ -4,15 +4,15 @@ from discord.ext import commands
 class bane(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-
 	
 	@commands.command()
 	async def bane(self, ctx, user=None):
 		if not user:
 			user = ctx.author.mention
-		embed=discord.Embed(description=f"{user} was banned")
+		embed=discord.Embed(description=f"{user} was bannned!")
 		await ctx.send(embed=embed)
-
+#Custom bane command, mentions whoever was mentioned in the bane msg and outputs: "was bannned."
 
 def setup(bot):
 	bot.add_cog(bane(bot))
+#Adds this as a cog to the bot.
