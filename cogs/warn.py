e848c733b9410562ba94543debb16cf506f16713
Subzeero <zeerommelaere@gmail.com>
2021-07-10 14:55:48 -0600

    Migrate 'delwarn' to new database

--- cogs/warn.py
+++ cogs/warn.py
@@ -40,32 +40,36 @@ class warning(commands.Cog):
 			'reason': reason
 		})
 
-		Database.update('warns', {'guild_id':ctx.guild.id}, {'$set': {'warn_data': warns['warn_data']}})
+		Database.update('warns', {'guild_id':ctx.guild.id}, {'$set':{'warn_data':warns['warn_data']}})
 
 		embed=discord.Embed(description=f"{user} Warned for: {reason}",color=0x06c258)
 		embed.set_author(name=user.name +'#'+ user.discriminator, icon_url=user.avatar_url)
 		await ctx.send(embed=embed)
 
-	# @commands.command(Administrator=True, aliases=['dw'] )
-	# async def delwarn(self, ctx, user:discord.User, warnId:int): 
-	# 	# with open('./warns.json', 'r') as f:
-	# 	# 	warns = json.load(f)
+	@commands.command(Administrator=True, aliases=['dw'] )
+	async def delwarn(self, ctx, user:discord.User, warnId:int):
+		warns = Database.find('warns', {'guild_id': ctx.guild.id})
 
-	# 		if not str(ctx.guild.id) in warns:
-	# 			warns[str(ctx.guild.id)] = {}
+		if not warns:
+			warns = {
+				'guild_id': ctx.guild.id,
+				'warn_data': {}
+			}
+			result = Database.insert('warns', warns)
+			warns = Database.find('warns', {'_id':result.inserted_id})
+			return await ctx.send('There are no warnings for this user.')
 
-	# 		if not str(user.id) in warns[str(ctx.guild.id)].keys():
-	# 			warns[str(ctx.guild.id)][str(user.id)] = []
-	# 			return await ctx.send("This user has no warnings!")
+		if not str(user.id) in warns['warn_data'].keys():
+			warns['warn_data'][str(user.id)] = []
+			return await ctx.send('There are no warnings for this user.')
 
-	# 		try:
-	# 			del warns[str(ctx.guild.id)][str(user.id)][warnId - 1]
-	# 		except:
-	# 			return await ctx.send("Invalid warning ID given, please check the ID and try again.")
-	# 		else:
-	# 			await ctx.send(f"Warn `{warnId}` has been removed.")
-	# 			# with open('./warns.json', 'w') as f: 
-	# 			# 	json.dump(warns, f, indent=4)
+		try:
+			del warns['warn_data'][str(user.id)][warnId - 1]
+		except:
+			return await ctx.send("Invalid warning ID given, please check the ID and try again.")
+		else:
+			Database.update('warns', {'guild_id':ctx.guild.id}, {'$set': {'warn_data': warns['warn_data']}})
+			return await ctx.send(f"Warn `{warnId}` has been removed.")
 	
 	@commands.command(Administrator=True, aliases=['warnings'])
 	async def warns(self, ctx, user:discord.User): 
