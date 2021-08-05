7129566314e49ce72fd71e66c635f212540fb00d
Subzeero <zeerommelaere@gmail.com>
2021-08-05 00:47:40 -0600

    Return prefix on ping

---
+++ cogs/onmessageping.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+class onmessageping(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.Cog.listener()
+	async def on_message(self, message):
+		if message.content == "<@!851275243157979166>":
+			prefix = await self.bot.get_prefix(message)
+			embed=discord.Embed(description=f"My current prefix is `{prefix}`\n\nTo change my prefix run `{prefix}changeprefix <prefix>`", color=0x06c258)
+			await message.reply(embed=embed)
+
+def setup(bot):
+	bot.add_cog(onmessageping(bot))
